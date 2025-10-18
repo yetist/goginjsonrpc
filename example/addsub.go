fc230b1e51cb14916a1675bbdc67cbb8ee351cd1
Wu Xiaotian <yetist@gmail.com>
2025-10-18 21:17:03 +0800

    example: use github.com/yetist/goginjsonrpc

--- example/addsub.go
+++ example/addsub.go
@@ -1,30 +1,29 @@
 package main
 
 import (
-  "github.com/kanocz/goginjsonrpc"
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"github.com/yetist/goginjsonrpc"
 )
 
 type TestRPC struct {
-  counter int
+	counter int
 }
 
 func (t *TestRPC) Add(arg int) int {
-  t.counter += arg
-  return t.counter
+	t.counter += arg
+	return t.counter
 }
 
 func (t *TestRPC) Sub(arg int) int {
-  t.counter += arg
-  return t.counter
+	t.counter += arg
+	return t.counter
 }
 
 func main() {
+	rpc := TestRPC{}
 
-  rpc := TestRPC{}
+	router := gin.Default()
+	router.POST("/", func(c *gin.Context) { goginjsonrpc.ProcessJsonRPC(c, &rpc) })
 
-  router := gin.Default()
-  router.POST("/", func(c *gin.Context) { goginjsonrpc.ProcessJsonRPC(c, &rpc); })
-
-  router.Run("127.0.0.1:8000")
+	router.Run("127.0.0.1:8000")
 }
